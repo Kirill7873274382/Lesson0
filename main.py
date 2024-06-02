a277547037099dd504adb759a4c8bf1e799da20a
Kirill7873274382 <kindofmasterwork@gmail.com>
2024-06-02 17:01:47 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
-print(1++1)
-print(type(4))
-print(bool(True))
-def Main():
-    return "gtgt"
-print(Main())
-print(type(int("2")))
+a = "dwd"
+print(len(a))
+#1 задние
+
+first = 1
+second = 2
+third = 3
+#2 задание
+
+summa = print(second+first)
+diff = print(second-first)
+
+#3 задание
+mean = print(first+second+third/first+second+third)
+
+#4 задание
+first_string = "Понедельник "
+second_string = "Вторник"
+third = print((first_string) + (second_string))
+
+#5 Задание
+a = 2
+b = 1
+c = 3
+f = ((a * b) + (a * c))
+print(f * 3 / 2)
 
 
